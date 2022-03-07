7caa98834d131ebeef43799c171b4cdb02e7b9db
JakeCollegeBoi <jf471719@ohio.edu>
2022-03-06 21:15:37 -0500

    Final Game Changes
    The Final Game is in Builds/Project Hedgemaze

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -35,7 +35,7 @@ public class PlayerController : MonoBehaviour
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
+        countText.text = "Keys: " + count.ToString();
         if(count >= 13)
         {
             winTextObject.SetActive(true);
